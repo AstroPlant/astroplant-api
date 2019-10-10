17817e687775c296661b2667f407d761274e1bd5
Thomas Churchman <thomas@kepow.org>
2019-10-10 14:08:42 +0200

    Bump Rust version.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rust-channel = (nixpkgs.rustChannelOf { date="2019-07-15"; channel = "nightly"; });
+  rust-channel = (nixpkgs.rustChannelOf { date="2019-10-03"; channel = "nightly"; });
   my-rust = rust-channel.rust.override { extensions = [ "rust-src" ]; };
   my-rust-src = rust-channel.rust-src;
 in

1c21ad78a9bc295d1503137e5cafbf15e5520a18
Thomas Churchman <thomas@kepow.org>
2019-11-08 19:56:21 +0100

    Specify nixpkgs-mozilla version.

--- shell.nix
+++ shell.nix
@@ -1,30 +1,37 @@
+with import <nixpkgs> {};
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rust-channel = (nixpkgs.rustChannelOf { date="2019-10-03"; channel = "nightly"; });
-  my-rust = rust-channel.rust.override { extensions = [ "rust-src" ]; };
-  my-rust-src = rust-channel.rust-src;
+  nixpkgs-mozilla = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "d46240e8755d91bc36c0c38621af72bf5c489e13";
+    sha256 = "0icws1cbdscic8s8lx292chvh3fkkbjp571j89lmmha7vl2n71jg";
+  };
 in
-  with import <nixpkgs> {};
-  pkgs.mkShell {
-    buildInputs = [
-      my-rust
-      my-rust-src
-      rustracer
-      pkgconfig
-      cmake
-      bashInteractive
-      cacert
-      openssl
-      postgresql_11
-      capnproto
-      # note: install diesel_cli with:
-      # cargo install diesel_cli --no-default-features --features postgres
-    ];
+  with import "${nixpkgs-mozilla.out}/rust-overlay.nix" pkgs pkgs;
+  let
+    rust-channel = (rustChannelOf { date="2019-11-03"; channel = "nightly"; });
+    my-rust = rust-channel.rust.override { extensions = [ "rust-src" ]; };
+    my-rust-src = rust-channel.rust-src;
+  in
+    pkgs.mkShell {
+      buildInputs = [
+        my-rust
+        my-rust-src
+        rustracer
+        pkgconfig
+        cmake
+        bashInteractive
+        cacert
+        openssl
+        postgresql_11
+        capnproto
+        # note: install diesel_cli with:
+        # cargo install diesel_cli --no-default-features --features postgres
+      ];
 
-    shellHook = ''
-      export RUST_SRC_PATH="${my-rust-src}/lib/rustlib/src/rust/src"
-      export RUST_LOG=warn,astroplant_rs_api=trace
-      export PATH="$HOME/.cargo/bin:$PATH"
-    '';
-  }
+      shellHook = ''
+        export RUST_SRC_PATH="${my-rust-src}/lib/rustlib/src/rust/src"
+        export RUST_LOG=warn,astroplant_rs_api=trace
+        export PATH="$HOME/.cargo/bin:$PATH"
+      '';
+    }

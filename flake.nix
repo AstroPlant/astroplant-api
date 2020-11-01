9ed59d8e920d19e191d8ef6ebec3e4ad4d5bab12
Thomas Churchman <thomas@kepow.org>
2020-11-01 18:52:50 +0200

    Add compatibility for non-flake-enabled systems

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "An API to interface with the AstroPlant back-end";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {

e1725197ec2e988f5c78e33c13d727c1492eeeb4
Thomas Churchman <thomas@kepow.org>
2023-09-01 15:31:03 +0200

    Restrict media parent deletion to prevent leakage

---
+++ migrations/2023-09-01-125329_dont-leak-media/up.sql
@@ -0,0 +1,12 @@
+-- Drop constraints
+ALTER TABLE media
+    DROP CONSTRAINT media_peripheral_id_fkey,
+    DROP CONSTRAINT media_kit_id_fkey,
+    DROP CONSTRAINT media_kit_configuration_id_fkey;
+
+-- Recreate constraints
+ALTER TABLE media
+    ADD CONSTRAINT media_peripheral_id_fkey FOREIGN KEY (peripheral_id) REFERENCES peripherals (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    ADD CONSTRAINT media_kit_id_fkey FOREIGN KEY (kit_id) REFERENCES kits (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    ADD CONSTRAINT media_kit_configuration_id_fkey FOREIGN KEY (kit_configuration_id) REFERENCES kit_configurations (id) ON DELETE RESTRICT ON UPDATE CASCADE;
+
